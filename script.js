40ed93178b457000d76ee2133e2487b3e6c88cf4
Rounack Sarkar <rounacksarkar@gmail.com>
2025-03-12 21:17:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function appendValue(value) {
+    if (value==="x"){
+        value="*"
+        document.getElementById.value
+    }
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        let display = document.getElementById('display');
+        display.value = eval(display.value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
